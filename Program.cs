9392aa9deda5c4dfeea04d2f78579ba1299df23a
RichardGiani <97562117+RichardGiani@users.noreply.github.com>
2022-01-11 20:41:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace projetoteste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia.");
+            Console.WriteLine("Como você está hoje?");
+
+            Console.ReadLine();
+        }
+    }
+}
